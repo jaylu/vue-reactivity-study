f0df033fea09e94bb394be5e4204902e5b133543
jaylu <jayjjlu@gmail.com>
2020-05-03 08:43:46 +0800

    resume reactive.js and simple-vue.js

--- web/modules/reactive.js
+++ web/modules/reactive.js
@@ -1,15 +1,137 @@
-export function reactive(target) {}
+import {mapValue} from '../modules/utils.js'
 
-export function isReactive(object) {}
+export class Dep {
+    constructor() {
+        this.subscribers = []
+    }
 
-export function ref(initValue) {}
+    register(callback) {
+        if (!this.subscribers.includes(callback)) {
+            this.subscribers.push(callback)
+        }
+    }
 
-export function isRef(object) {}
+    notify(oldValue, newValue) {
+        this.subscribers.forEach(callback => {
+            callback(oldValue, newValue)
+        })
+    }
+}
 
-export function toRef(object, key) {}
+let trackingFunction = null;
 
-export function toRefs(object) {}
 
-export function watchEffect(fn) {}
+function reactiveES5(target) {
+    if (target.__isReactive) {
+        return target
+    }
+    Object.entries(target).forEach(([key, value]) => {
+        const dep = new Dep()
+        let internalValue = typeof value === 'object' ? reactiveES5(value) : value
+        Object.defineProperty(target, key, {
+            enumerable: true,
+            get() {
+                if (trackingFunction) {
+                    dep.register(trackingFunction)
+                }
+                return internalValue
+            },
+            set(newValue) {
+                let oldValue = internalValue
+                internalValue = typeof newValue === 'object' ? reactive(newValue) : newValue
+                dep.notify(oldValue, newValue)
+            }
+        })
+    })
+    target.__isReactive = true
+    return target
+}
 
-export function computed(fn) {}
+function reactiveES6(targetParam) {
+    if (targetParam.__isReactive) {
+        return targetParam
+    }
+
+    const keyToDepMap = new Map()
+    function getDepMapByKey(key) {
+        if (!keyToDepMap.has(key)){
+            keyToDepMap.set(key, new Dep())
+        }
+        return keyToDepMap.get(key)
+    }
+
+    let proxy = new Proxy({
+        ...mapValue(targetParam, value => (typeof value === 'object') ? reactiveES6(value): value)
+    }, {
+        get(target, key){
+            if (trackingFunction) {
+                const dep = getDepMapByKey(key);
+                dep.register(trackingFunction)
+            }
+            return target[key]
+        },
+        set(target, key, newValue) {
+            let oldValue = target[key]
+            target[key] = newValue
+            const dep = getDepMapByKey(key);
+            dep.notify(oldValue, newValue)
+            return true
+        }
+    })
+
+    proxy.__isReactive = true
+    return proxy;
+
+}
+
+export const reactive = reactiveES6
+
+export function isReactive(object) {
+    return object !== null && object !== undefined && object.__isReactive
+}
+
+export function ref(initValue) {
+    let result = reactive({
+        value: initValue
+    })
+    result.__isRef = true
+    return result
+}
+
+export function isRef(object) {
+    return object !== null && object !== undefined && object.__isRef
+}
+
+export function toRef(object, key) {
+    return {
+        __isRef: true,
+        get value() {
+            return object[key]
+        },
+        set value(newValue) {
+            object[key] = newValue
+        }
+    }
+}
+
+export function toRefs(object) {
+    let result = {}
+    Object.keys(object).forEach((key) => {
+        result[key] = toRef(object, key)
+    })
+    return result
+}
+
+export function watchEffect(fn) {
+    trackingFunction = fn
+    fn()
+    trackingFunction = null
+}
+
+export function computed(fn) {
+    let reference = ref()
+    watchEffect(() => {
+        reference.value = fn()
+    })
+    return reference
+}
--- web/modules/simple-vue.js
+++ web/modules/simple-vue.js
@@ -1,4 +1,26 @@
-export * from './reactive.js'
+import { inflate } from './utils.js'
+import {watchEffect} from './reactive.js'
 
-export function createApp(options) {Ø
+export * from './reactive.js'
+export function createApp(options) {
+    const mount = (selector) => {
+        const { template, setup } = options
+        const model = setup()
+
+        const render = () => {
+            const renderedDom = inflate(template, model)
+            const dom = document.querySelector(selector)
+            if (dom) {
+                dom.innerHTML = renderedDom
+            }
+        }
+
+        watchEffect(() => {
+            render()
+        })
+
+        return {
+            ...model
+        }
+    }
 }
